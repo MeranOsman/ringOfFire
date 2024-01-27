316ca636f002a15bc651ec5f646e7db5c4a7acef
Meran Osman <meran-osman@gmx.de>
2024-01-27 15:21:06 +0100

    current

---
+++ src/app/game/game.component.ts
@@ -0,0 +1,49 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [
+    CommonModule,
+    PlayerComponent,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss'
+})
+export class GameComponent {
+  pickCardAnimation = false;
+  currentCard: string | any = '';
+  game: Game = new Game();
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log(this.game);
+  }
+
+
+  newGame() {
+    this.game;
+  }
+
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      console.log('new Card:' + this.currentCard);
+      console.log('game is', this.game);
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+}
